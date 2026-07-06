0177390c9f2c0f23968f8175efc84120669483f2
FahrandRipa <muhammadfahrandripa@gmail.com>
2026-07-06 14:49:19 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function lihatJawaban() {
+    document.getElementById("result").innerHTML = "Tidak";
+    result.style.color = "red";
+}
+
+function tidakMau() {
+    document.getElementById("result").innerHTML = "Kamu cukup handal dalam percaya diri";
+    result.style.color = "white";
+}
